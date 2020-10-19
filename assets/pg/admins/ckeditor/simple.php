5d6401a47241fee0a9f0b20c651441606cc57931
Hassini Ayoub <40410402+hassiniayoub@users.noreply.github.com>
2020-10-19 13:47:45 +0100

    Add files via upload

---
+++ assets/pg/admins/ckeditor/simple.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <textarea name="editor1" id="editor1" cols="30" rows="10"></textarea>
+
+
+    <script src="ckeditor.js"></script>
+    <script>
+        // Replace the <textarea id="editor1"> with a CKEditor 4
+        // instance, using default configuration.
+        CKEDITOR.replace('editor1');
+        CKEDITOR.editorConfig = function( config ) {
+            config.language = 'ar';
+            config.uiColor = '#F7B42C';
+            config.height = 300;
+            config.toolbarCanCollapse = true;
+        };
+    </script>
+</body>
+</html>
